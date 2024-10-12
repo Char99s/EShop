1cab1a80c609c1627c31491aebea30c4645a3d17
Char99s <103144686+Char99s@users.noreply.github.com>
2024-10-12 23:14:50 +0300

    ✨ feat(Catalog API): New features
    Add Update and Delete product features
    Add command validation pipeline using FluentValidation

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -5,10 +5,24 @@
         : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid id);
 
-    internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+            RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+        }
+    }
+
+    internal class CreateProductCommandHandler(IDocumentSession session, ILogger<CreateProductCommandHandler> logger) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
+
+            logger.LogInformation("CreateProductCommandHandler.Handle called with {@Command}", command);
+
             // Create product entity from command object
 
             var product = new Product
@@ -20,7 +34,6 @@
                 Price = command.Price,
             };
 
-            // Todo
             // Save to database
             session.Store(product);
             await session.SaveChangesAsync(cancellationToken);
